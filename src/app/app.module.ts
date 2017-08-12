9b203a528dd7631a341aa236d8033eb900f5dd63
Chelsea-li <miemie1018@gmail.com>
2017-08-12 14:36:39 -0700

    add add-client form with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router'
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 //AngularFire Imports
 import{AngularFireModule} from 'angularfire2';
@@ -26,7 +27,8 @@ import {ClientService} from './services/client.service';
 const appRoutes:Routes = [
   {path:'', component: DashboardComponent},
   {path:'register', component: RegisterComponent},
-  {path:'login', component: LoginComponent}
+  {path:'login', component: LoginComponent},
+  {path:'add-client', component: AddClientComponent}
 ];
 
 export const firebaseConfig ={
@@ -55,7 +57,8 @@ export const firebaseConfig ={
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig),
+    FormsModule
   ],
   providers: [
     AngularFireAuth,
