70f940ad4e58269a9d872b0484ef7396057f0c44
Chelsea-li <miemie1018@gmail.com>
2017-08-11 17:45:50 -0700

    initial add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,67 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router'
 
+//AngularFire Imports
+import{AngularFireModule} from 'angularfire2';
+import{AngularFireDatabase} from 'angularfire2/database';
+import{AngularFireAuth} from 'angularfire2/auth';
+//component imports
 import { AppComponent } from './app.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+//service imports
+import {ClientService} from './services/client.service';
+
+
+const appRoutes:Routes = [
+  {path:'', component: DashboardComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'login', component: LoginComponent}
+];
+
+export const firebaseConfig ={
+  apiKey: "AIzaSyB_X716SbtyqqwJQszmDA-YOv7Wxe7wLC0",
+  authDomain: "clientmanager-ded6a.firebaseapp.com",
+  databaseURL: "https://clientmanager-ded6a.firebaseio.com",
+  storageBucket: "clientmanager-ded6a.appspot.com",
+  messagingSenderId: "777396541802"  
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddClientComponent,
+    ClientDetailsComponent,
+    ClientsComponent,
+    DashboardComponent,
+    EditClientComponent,
+    LoginComponent,
+    NavbarComponent,
+    PageNotFoundComponent,
+    RegisterComponent,
+    SettingsComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase,
+    ClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
